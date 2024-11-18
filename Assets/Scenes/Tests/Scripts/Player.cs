41a3aa427339d48207daa8e9d041cb7c92b871b8
DESKTOP-RCIVMR8\norbstar <richard.norbron@gmail.com>
2024-11-18 14:57:58 +0000

    Added namespace references to a bunch of scripts.

--- Assets/Scenes/Tests/Scripts/Player.cs
+++ Assets/Scenes/Tests/Scripts/Player.cs
@@ -5,279 +5,282 @@ using UnityEngine.InputSystem;
 
 using UI;
 
-[RequireComponent(typeof(AudioSource))]
-public class Player : MonoBehaviour
+namespace Tests
 {
-    [Header("Components")]
-    [SerializeField] OnTrigger2DHandler groundTrigger;
-
-    [Header("Audio")]
-    [SerializeField] AudioClip jumpClip;
-    [SerializeField] AudioClip landClip;
-
-    [Header("UI")]
-    [SerializeField] AttributeUI isGroundedUI;
-    [SerializeField] AttributeUI velocityXUI;
-    [SerializeField] AttributeUI velocityYUI;
-    [SerializeField] AttributeUI stateUI;
-
-    [Header("Move")]
-    [Range(10f, 50f)]
-    [SerializeField] float moveSpeed = 25f;
-
-    [Header("Jump")]
-    [Range(0f, 250f)]
-    [SerializeField] float minJumpSpeed = 250f;
-    [Range(250f, 500f)]
-    [SerializeField] float midJumpSpeed = 500f;
-    [Range(500f, 750f)]
-    [SerializeField] float maxJumpSpeed = 750f;
-
-    [Header("Dash")]
-    [SerializeField] float dashSpeed = 100f;
-
-    [Header("Move Stats")]
-    [SerializeField] float moveXValue;
-    [SerializeField] float trueMoveXValue;
-    [SerializeField] float moveYValue;
-    [SerializeField] float trueMoveYValue;
-
-    [Header("Rigidbody")]
-    [SerializeField] float velocityX;
-    [SerializeField] float velocityY;
-
-    [Header("Inferences")]
-    [SerializeField] bool isGrounded;
-
-    private InputSystem_Actions inputActions;
-    private Rigidbody2D rigidBody;
-    private AudioSource audioSource;
-    private PlayerState state;
-
-    private bool execRun, execJump/*, execPowerJump*/, execDash;
-    private float jumpPressStartTime, jumpHeldDuration;
-
-    void Awake()
+    [RequireComponent(typeof(AudioSource))]
+    public class Player : MonoBehaviour
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        audioSource = GetComponent<AudioSource>();
-        inputActions = new InputSystem_Actions();
-    }
+        [Header("Components")]
+        [SerializeField] OnTrigger2DHandler groundTrigger;
 
-    void OnEnable()
-    {
-        inputActions.Enable();
+        [Header("Audio")]
+        [SerializeField] AudioClip jumpClip;
+        [SerializeField] AudioClip landClip;
 
-        // inputActions.Player.Move.performed += OnMoveIntent;
-        // inputActions.Player.Jump.performed += OnJumpIntent;
-        // inputActions.Player.PowerJump.performed += OnPowerJumpIntent;
-        inputActions.Player.JumpPress.performed += OnJumpPressIntent;
-        inputActions.Player.JumpRelease.performed += OnJumpReleaseIntent;
-        inputActions.Player.Dash.performed += OnDashIntent;
+        [Header("UI")]
+        [SerializeField] AttributeUI isGroundedUI;
+        [SerializeField] AttributeUI velocityXUI;
+        [SerializeField] AttributeUI velocityYUI;
+        [SerializeField] AttributeUI stateUI;
 
-        groundTrigger.Register(new OnTrigger2DHandler.Events
+        [Header("Move")]
+        [Range(10f, 50f)]
+        [SerializeField] float moveSpeed = 25f;
+
+        [Header("Jump")]
+        [Range(0f, 250f)]
+        [SerializeField] float minJumpSpeed = 250f;
+        [Range(250f, 500f)]
+        [SerializeField] float midJumpSpeed = 500f;
+        [Range(500f, 750f)]
+        [SerializeField] float maxJumpSpeed = 750f;
+
+        [Header("Dash")]
+        [SerializeField] float dashSpeed = 100f;
+
+        [Header("Move Stats")]
+        [SerializeField] float moveXValue;
+        [SerializeField] float trueMoveXValue;
+        [SerializeField] float moveYValue;
+        [SerializeField] float trueMoveYValue;
+
+        [Header("Rigidbody")]
+        [SerializeField] float velocityX;
+        [SerializeField] float velocityY;
+
+        [Header("Inferences")]
+        [SerializeField] bool isGrounded;
+
+        private InputSystem_Actions inputActions;
+        private Rigidbody2D rigidBody;
+        private AudioSource audioSource;
+        private PlayerState state;
+
+        private bool execRun, execJump/*, execPowerJump*/, execDash;
+        private float jumpPressStartTime, jumpHeldDuration;
+
+        void Awake()
         {
-            Gained = OnGround,
-            Lost = OffGround
-        });
-    }
-
-    void OnDisable() => inputActions.Disable();
-
-    // private void OnMoveIntent(InputAction.CallbackContext context)
-    // {
-    //     var rawValue = inputActions.Player.Move.ReadValue<Vector2>();
-    //     moveXValue = rawValue.x;
-    //     moveYValue = rawValue.y;
-
-    //     var value = rawValue * Time.deltaTime * moveSpeed;
-    //     trueMoveXValue = value.x;
-    //     trueMoveYValue = value.y;
-
-    //     if (isGrounded && Mathf.Abs(trueMoveXValue) > Mathf.Epsilon)
-    //     {
-    //         execRun = true;
-    //     }
-    // }
-
-    // private void OnJumpIntent(InputAction.CallbackContext context)
-    // {
-    //     Debug.Log("OnJumpIntent");
-
-    //     if (isGrounded)
-    //     {
-    //         exeJump = true;
-    //     }
-    // }
-
-    // private void OnPowerJumpIntent(InputAction.CallbackContext context)
-    // {
-    //     Debug.Log("OnPowerJumpIntent");
-
-    //     if (isGrounded)
-    //     {
-    //         execPowerJump = true;
-    //     }
-    // }
-
-    private void OnJumpPressIntent(InputAction.CallbackContext context)
-    {
-        // Debug.Log("OnJumpPressIntent");
-        jumpPressStartTime = Time.time;
-    }
-
-    private void OnJumpReleaseIntent(InputAction.CallbackContext context)
-    {
-        // Debug.Log("OnJumpReleaseIntent");
-        jumpHeldDuration = Time.time - jumpPressStartTime;
-        execJump = true;
-    }
-
-    private void OnDashIntent(InputAction.CallbackContext context)
-    {
-        // Debug.Log("OnDashIntent");
-
-        if (isGrounded)
-        {
-            execDash = true;
-        }
-    }
-
-    private void EvalualRawIntents()
-    {
-        var rawValue = inputActions.Player.Move.ReadValue<Vector2>();
-        moveXValue = rawValue.x;
-        moveYValue = rawValue.y;
-
-        var value = rawValue * Time.deltaTime * moveSpeed;
-        trueMoveXValue = value.x;
-        trueMoveYValue = value.y;
-
-        OnRunIntent();
-    }
-
-    private void OnRunIntent()
-    {
-        if (isGrounded && Mathf.Abs(trueMoveXValue) > Mathf.Epsilon)
-        {
-            execRun = true;
-        }
-    }
-
-    private void UpdateUI()
-    {
-        isGroundedUI.Value = isGrounded ? "True" : "False";
-
-        velocityX = rigidBody.linearVelocityX;
-        velocityY = rigidBody.linearVelocityY;
-
-        velocityXUI.Value = velocityX.ToString();
-        velocityYUI.Value = velocityY.ToString();
-
-        stateUI.Value = state.ToString();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        EvalualRawIntents();
-        UpdateUI();
-    }
-
-    private void ApplyRun()
-    {
-        rigidBody.linearVelocityX = trueMoveXValue * moveSpeed;
-        execRun = false;
-    }
-
-    // private void ApplyJump()
-    // {
-    //     rigidBody.AddForce(Vector2.up * minJumpSpeed);
-    //     execJump = false;
-    // }
-
-    // private void ApplyPowerJump()
-    // {
-    //     rigidBody.AddForce(Vector2.up * maxJumpSpeed);
-    //     execPowerJump = false;
-    // }
-
-    private void ApplyJump(float heldDuration)
-    {
-        if (heldDuration > 0.4f)
-        {
-            rigidBody.AddForce(Vector2.up * maxJumpSpeed);
-        }
-        else if (heldDuration > 0.2f)
-        {
-            rigidBody.AddForce(Vector2.up * midJumpSpeed);
-        }
-        else
-        {
-            rigidBody.AddForce(Vector2.up * minJumpSpeed);
+            rigidBody = GetComponent<Rigidbody2D>();
+            audioSource = GetComponent<AudioSource>();
+            inputActions = new InputSystem_Actions();
         }
 
-        execJump = false;
-    }
-
-    private void ApplyDash()
-    {
-        var direction = Mathf.Sign(rigidBody.linearVelocityX);
-        rigidBody.linearVelocityX = direction * dashSpeed;
-        execDash = false;
-    }
-
-    public void OnGround(Collider2D collider)
-    {
-        audioSource.PlayOneShot(landClip, 1f);
-        isGrounded = true;
-    }
-
-    public void OffGround(Collider2D collider)
-    {
-        audioSource.PlayOneShot(jumpClip, 1f);
-        isGrounded = false;
-    }
-
-    void FixedUpdate()
-    {
-        state = PlayerState.Idle;
-
-        if (rigidBody.linearVelocity.y > Mathf.Epsilon)
+        void OnEnable()
         {
-            state = PlayerState.Jumping;
-        }
-        else if (rigidBody.linearVelocity.y < -Mathf.Epsilon)
-        {
-            state = PlayerState.Falling;
+            inputActions.Enable();
+
+            // inputActions.Player.Move.performed += OnMoveIntent;
+            // inputActions.Player.Jump.performed += OnJumpIntent;
+            // inputActions.Player.PowerJump.performed += OnPowerJumpIntent;
+            inputActions.Player.JumpPress.performed += OnJumpPressIntent;
+            inputActions.Player.JumpRelease.performed += OnJumpReleaseIntent;
+            inputActions.Player.Dash.performed += OnDashIntent;
+
+            groundTrigger.Register(new OnTrigger2DHandler.Events
+            {
+                Gained = OnGround,
+                Lost = OffGround
+            });
         }
 
-        if(execRun)
-        {
-            ApplyRun();
-            state = PlayerState.Running;
-        }
+        void OnDisable() => inputActions.Disable();
 
-        // if (execJump)
+        // private void OnMoveIntent(InputAction.CallbackContext context)
         // {
-        //     ApplyJump();
+        //     var rawValue = inputActions.Player.Move.ReadValue<Vector2>();
+        //     moveXValue = rawValue.x;
+        //     moveYValue = rawValue.y;
+
+        //     var value = rawValue * Time.deltaTime * moveSpeed;
+        //     trueMoveXValue = value.x;
+        //     trueMoveYValue = value.y;
+
+        //     if (isGrounded && Mathf.Abs(trueMoveXValue) > Mathf.Epsilon)
+        //     {
+        //         execRun = true;
+        //     }
         // }
 
-        // if (execPowerJump)
+        // private void OnJumpIntent(InputAction.CallbackContext context)
         // {
-        //     ApplyPowerJump();
+        //     Debug.Log("OnJumpIntent");
+
+        //     if (isGrounded)
+        //     {
+        //         exeJump = true;
+        //     }
         // }
 
-        if (execJump)
+        // private void OnPowerJumpIntent(InputAction.CallbackContext context)
+        // {
+        //     Debug.Log("OnPowerJumpIntent");
+
+        //     if (isGrounded)
+        //     {
+        //         execPowerJump = true;
+        //     }
+        // }
+
+        private void OnJumpPressIntent(InputAction.CallbackContext context)
         {
-            ApplyJump(jumpHeldDuration);
+            // Debug.Log("OnJumpPressIntent");
+            jumpPressStartTime = Time.time;
         }
 
-        
-        if (execDash)
+        private void OnJumpReleaseIntent(InputAction.CallbackContext context)
         {
-            ApplyDash();
-            state = PlayerState.Dashing;
+            // Debug.Log("OnJumpReleaseIntent");
+            jumpHeldDuration = Time.time - jumpPressStartTime;
+            execJump = true;
+        }
+
+        private void OnDashIntent(InputAction.CallbackContext context)
+        {
+            // Debug.Log("OnDashIntent");
+
+            if (isGrounded)
+            {
+                execDash = true;
+            }
+        }
+
+        private void EvalualRawIntents()
+        {
+            var rawValue = inputActions.Player.Move.ReadValue<Vector2>();
+            moveXValue = rawValue.x;
+            moveYValue = rawValue.y;
+
+            var value = rawValue * Time.deltaTime * moveSpeed;
+            trueMoveXValue = value.x;
+            trueMoveYValue = value.y;
+
+            OnRunIntent();
+        }
+
+        private void OnRunIntent()
+        {
+            if (isGrounded && Mathf.Abs(trueMoveXValue) > Mathf.Epsilon)
+            {
+                execRun = true;
+            }
+        }
+
+        private void UpdateUI()
+        {
+            isGroundedUI.Value = isGrounded ? "True" : "False";
+
+            velocityX = rigidBody.linearVelocityX;
+            velocityY = rigidBody.linearVelocityY;
+
+            velocityXUI.Value = velocityX.ToString("0.00");
+            velocityYUI.Value = velocityY.ToString("0.00");
+
+            stateUI.Value = state.ToString();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            EvalualRawIntents();
+            UpdateUI();
+        }
+
+        private void ApplyRun()
+        {
+            rigidBody.linearVelocityX = trueMoveXValue * moveSpeed;
+            execRun = false;
+        }
+
+        // private void ApplyJump()
+        // {
+        //     rigidBody.AddForce(Vector2.up * minJumpSpeed);
+        //     execJump = false;
+        // }
+
+        // private void ApplyPowerJump()
+        // {
+        //     rigidBody.AddForce(Vector2.up * maxJumpSpeed);
+        //     execPowerJump = false;
+        // }
+
+        private void ApplyJump(float heldDuration)
+        {
+            if (heldDuration > 0.4f)
+            {
+                rigidBody.AddForce(Vector2.up * maxJumpSpeed);
+            }
+            else if (heldDuration > 0.2f)
+            {
+                rigidBody.AddForce(Vector2.up * midJumpSpeed);
+            }
+            else
+            {
+                rigidBody.AddForce(Vector2.up * minJumpSpeed);
+            }
+
+            execJump = false;
+        }
+
+        private void ApplyDash()
+        {
+            var direction = Mathf.Sign(rigidBody.linearVelocityX);
+            rigidBody.linearVelocityX = direction * dashSpeed;
+            execDash = false;
+        }
+
+        public void OnGround(Collider2D collider)
+        {
+            audioSource.PlayOneShot(landClip, 1f);
+            isGrounded = true;
+        }
+
+        public void OffGround(Collider2D collider)
+        {
+            audioSource.PlayOneShot(jumpClip, 1f);
+            isGrounded = false;
+        }
+
+        void FixedUpdate()
+        {
+            state = PlayerState.Idle;
+
+            if (rigidBody.linearVelocity.y > Mathf.Epsilon)
+            {
+                state = PlayerState.Jumping;
+            }
+            else if (rigidBody.linearVelocity.y < -Mathf.Epsilon)
+            {
+                state = PlayerState.Falling;
+            }
+
+            if(execRun)
+            {
+                ApplyRun();
+                state = PlayerState.Running;
+            }
+
+            // if (execJump)
+            // {
+            //     ApplyJump();
+            // }
+
+            // if (execPowerJump)
+            // {
+            //     ApplyPowerJump();
+            // }
+
+            if (execJump)
+            {
+                ApplyJump(jumpHeldDuration);
+            }
+
+            
+            if (execDash)
+            {
+                ApplyDash();
+                state = PlayerState.Dashing;
+            }
         }
     }
 }
--- Assets/Scenes/Tests/Scripts/TilemapTransition.cs
+++ Assets/Scenes/Tests/Scripts/TilemapTransition.cs
@@ -3,139 +3,142 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TilemapTransition : MonoBehaviour
+namespace Tests
 {
-    [SerializeField] Tilemap tilemap;
-    [SerializeField] Tilemap altTilemap;
-    [SerializeField] float transitionDuration = 0.5f;
-    // [SerializeField] float relativeSpeed = 1f;
-
-    [Header("Stats")]
-    [SerializeField] float elapsedTime;
-    [SerializeField] float time;
-    [SerializeField] float alpha;
-
-    private float relativeSpeed;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class TilemapTransition : MonoBehaviour
     {
-        relativeSpeed = 1f / transitionDuration;
-        StartCoroutine(Co_FadeInAltTilemap());
-    }
+        [SerializeField] Tilemap tilemap;
+        [SerializeField] Tilemap altTilemap;
+        [SerializeField] float transitionDuration = 0.5f;
+        // [SerializeField] float relativeSpeed = 1f;
 
-    private (float elapsed, float remaining) CalcTime(float startAlpha, float elapsedTime, float speed)
-    {
-        var t1 = 1f - startAlpha + elapsedTime * relativeSpeed;
-        var t2 = startAlpha + elapsedTime * relativeSpeed;
-        return (t1, t2);
-    }
+        [Header("Stats")]
+        [SerializeField] float elapsedTime;
+        [SerializeField] float time;
+        [SerializeField] float alpha;
 
-    private IEnumerator FadeOutTilemap(Tilemap thisTilemap)
-    {
-        var startAlpha = thisTilemap.color.a;
-        elapsedTime = 0;
+        private float relativeSpeed;
 
-        while (thisTilemap.color.a != 0f)
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            var time = 1f - startAlpha + elapsedTime * relativeSpeed;
-            // time = CalcTime(startAlpha, elapsedTime, relativeSpeed).elapsed;
-            alpha = Mathf.Lerp(1f, 0f, time);
-            thisTilemap.color = new Color(1f, 1f, 1f, alpha);
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            relativeSpeed = 1f / transitionDuration;
+            StartCoroutine(Co_FadeInAltTilemap());
         }
-    }
 
-    private IEnumerator FadeInTilemap(Tilemap thisTilemap)
-    {
-        var startAlpha = thisTilemap.color.a;
-        elapsedTime = 0;
-
-        while (thisTilemap.color.a != 1f)
+        private (float elapsed, float remaining) CalcTime(float startAlpha, float elapsedTime, float speed)
         {
-            var time = startAlpha + elapsedTime * relativeSpeed;
-            // time = CalcTime(startAlpha, elapsedTime, relativeSpeed).remaining;
-            alpha = Mathf.Lerp(0f, 1f, time);
-            thisTilemap.color = new Color(1f, 1f, 1f, alpha);
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            var t1 = 1f - startAlpha + elapsedTime * relativeSpeed;
+            var t2 = startAlpha + elapsedTime * relativeSpeed;
+            return (t1, t2);
         }
-    }
-    
-    // private IEnumerator Co_FadeInTilemap()
-    // {
-    //     var startAlpha = altTilemap.color.a;
-    //     elapsedTime = 0;
 
-    //     while (altTilemap.color.a != 0f)
-    //     {
-    //         // var time = 1f - startAlpha + elapsedTime * relativeSpeed;
-    //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).elapsed;
-    //         alpha = Mathf.Lerp(1f, 0f, time);
-    //         altTilemap.color = new Color(1f, 1f, 1f, alpha);
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
+        private IEnumerator FadeOutTilemap(Tilemap thisTilemap)
+        {
+            var startAlpha = thisTilemap.color.a;
+            elapsedTime = 0;
 
-    //     startAlpha = tilemap.color.a;
-    //     elapsedTime = 0;
+            while (thisTilemap.color.a != 0f)
+            {
+                var time = 1f - startAlpha + elapsedTime * relativeSpeed;
+                // time = CalcTime(startAlpha, elapsedTime, relativeSpeed).elapsed;
+                alpha = Mathf.Lerp(1f, 0f, time);
+                thisTilemap.color = new Color(1f, 1f, 1f, alpha);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
 
-    //     while (tilemap.color.a != 1f)
-    //     {
-    //         // var time = startAlpha + elapsedTime * relativeSpeed;
-    //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).remaining;
-    //         alpha = Mathf.Lerp(0f, 1f, time);
-    //         tilemap.color = new Color(1f, 1f, 1f, alpha);
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
+        private IEnumerator FadeInTilemap(Tilemap thisTilemap)
+        {
+            var startAlpha = thisTilemap.color.a;
+            elapsedTime = 0;
 
-    //     StartCoroutine(Co_FadeInAltTilemap());
-    // }
+            while (thisTilemap.color.a != 1f)
+            {
+                var time = startAlpha + elapsedTime * relativeSpeed;
+                // time = CalcTime(startAlpha, elapsedTime, relativeSpeed).remaining;
+                alpha = Mathf.Lerp(0f, 1f, time);
+                thisTilemap.color = new Color(1f, 1f, 1f, alpha);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+        
+        // private IEnumerator Co_FadeInTilemap()
+        // {
+        //     var startAlpha = altTilemap.color.a;
+        //     elapsedTime = 0;
 
-    // private IEnumerator Co_FadeInAltTilemap()
-    // {
-    //     var startAlpha = tilemap.color.a;
-    //     elapsedTime = 0;
+        //     while (altTilemap.color.a != 0f)
+        //     {
+        //         // var time = 1f - startAlpha + elapsedTime * relativeSpeed;
+        //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).elapsed;
+        //         alpha = Mathf.Lerp(1f, 0f, time);
+        //         altTilemap.color = new Color(1f, 1f, 1f, alpha);
+        //         elapsedTime += Time.deltaTime;
+        //         yield return null;
+        //     }
 
-    //     while (tilemap.color.a != 0f)
-    //     {
-    //         // var time = 1f - startAlpha + elapsedTime * relativeSpeed;
-    //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).elapsed;
-    //         alpha = Mathf.Lerp(1f, 0f, time);
-    //         tilemap.color = new Color(1f, 1f, 1f, alpha);
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
+        //     startAlpha = tilemap.color.a;
+        //     elapsedTime = 0;
 
-    //     startAlpha = altTilemap.color.a;
-    //     elapsedTime = 0;
+        //     while (tilemap.color.a != 1f)
+        //     {
+        //         // var time = startAlpha + elapsedTime * relativeSpeed;
+        //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).remaining;
+        //         alpha = Mathf.Lerp(0f, 1f, time);
+        //         tilemap.color = new Color(1f, 1f, 1f, alpha);
+        //         elapsedTime += Time.deltaTime;
+        //         yield return null;
+        //     }
 
-    //     while (altTilemap.color.a != 1f)
-    //     {
-    //         // var time = startAlpha + elapsedTime * relativeSpeed;
-    //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).remaining;
-    //         alpha = Mathf.Lerp(0f, 1f, time);
-    //         altTilemap.color = new Color(1f, 1f, 1f, alpha);
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
+        //     StartCoroutine(Co_FadeInAltTilemap());
+        // }
 
-    //     StartCoroutine(Co_FadeInTilemap());
-    // }
+        // private IEnumerator Co_FadeInAltTilemap()
+        // {
+        //     var startAlpha = tilemap.color.a;
+        //     elapsedTime = 0;
 
-    private IEnumerator Co_FadeInAltTilemap()
-    {
-        yield return FadeOutTilemap(tilemap);
-        yield return FadeInTilemap(altTilemap);
-        StartCoroutine(Co_FadeInTilemap());
-    }
+        //     while (tilemap.color.a != 0f)
+        //     {
+        //         // var time = 1f - startAlpha + elapsedTime * relativeSpeed;
+        //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).elapsed;
+        //         alpha = Mathf.Lerp(1f, 0f, time);
+        //         tilemap.color = new Color(1f, 1f, 1f, alpha);
+        //         elapsedTime += Time.deltaTime;
+        //         yield return null;
+        //     }
 
-    private IEnumerator Co_FadeInTilemap()
-    {
-        yield return FadeOutTilemap(altTilemap);
-        yield return FadeInTilemap(tilemap);
-        StartCoroutine(Co_FadeInAltTilemap());
+        //     startAlpha = altTilemap.color.a;
+        //     elapsedTime = 0;
+
+        //     while (altTilemap.color.a != 1f)
+        //     {
+        //         // var time = startAlpha + elapsedTime * relativeSpeed;
+        //         time = CalcTime(startAlpha, elapsedTime, relativeSpeed).remaining;
+        //         alpha = Mathf.Lerp(0f, 1f, time);
+        //         altTilemap.color = new Color(1f, 1f, 1f, alpha);
+        //         elapsedTime += Time.deltaTime;
+        //         yield return null;
+        //     }
+
+        //     StartCoroutine(Co_FadeInTilemap());
+        // }
+
+        private IEnumerator Co_FadeInAltTilemap()
+        {
+            yield return FadeOutTilemap(tilemap);
+            yield return FadeInTilemap(altTilemap);
+            StartCoroutine(Co_FadeInTilemap());
+        }
+
+        private IEnumerator Co_FadeInTilemap()
+        {
+            yield return FadeOutTilemap(altTilemap);
+            yield return FadeInTilemap(tilemap);
+            StartCoroutine(Co_FadeInAltTilemap());
+        }
     }
 }
